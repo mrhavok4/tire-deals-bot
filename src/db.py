acb47074abafbfc9f7bc47da3b2e847058bc0652
mrhavok4 <soaresvirtualempresa@gmail.com>
2026-01-22 16:11:47 -0300

    Update db.py

--- src/db.py
+++ src/db.py
@@ -29,9 +29,4 @@ def upsert_deal(conn: sqlite3.Connection, deal: Dict[str, Any]) -> bool:
         conn.commit()
         return True
     except sqlite3.IntegrityError:
-        conn.execute(
-            "UPDATE deals SET last_seen_at=CURRENT_TIMESTAMP WHERE url=? AND price_cents IS ?",
-            (deal["url"], deal.get("price_cents")),
-        )
-        conn.commit()
         return False
