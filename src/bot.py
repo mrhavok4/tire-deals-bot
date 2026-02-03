951107f67e559374559d359845db575819d76432
mrhavok4 <soaresvirtualempresa@gmail.com>
2026-02-03 10:23:05 -0300

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,8 @@
 import requests
 
 def send_telegram_message(bot_token: str, chat_id: str, text: str) -> None:
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     r = requests.post(
-        url,
+        f"https://api.telegram.org/bot{bot_token}/sendMessage",
         json={"chat_id": chat_id, "text": text, "disable_web_page_preview": True},
         timeout=30,
     )
